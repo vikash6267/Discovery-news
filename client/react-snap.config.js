4e6731f2717cb2d28df73a8a1d1a3c7c16aea6ec
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2024-09-15 21:12:08 +0530

    ..react snap

--- client/react-snap.config.js
+++ client/react-snap.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Specify the paths to pre-render
-    include: ["/", "/news/*"], // Adjust paths as needed
+    include: ["/", "/*"], // Adjust paths as needed
     // You can include specific pages you want to pre-render, like:
     // include: ["/", "/news/123", "/another-page"]
   };
