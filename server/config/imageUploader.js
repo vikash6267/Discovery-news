b0d3b92e3d88759a898923bbe3b8a9d6c1a3122a
Rishi <rishimaheshwari040@gmail.com>
2024-11-05 15:07:07 +0530

    update image

--- server/config/imageUploader.js
+++ server/config/imageUploader.js
@@ -1,14 +1,15 @@
-const cloudinary = require("cloudinary").v2
+const cloudinary = require("cloudinary").v2;
 
-exports.uploadImageToCloudinary = async (file, folder, height, quality) => {
-  const options = { folder }
-  if (height) {
-    options.height = height
-  }
-  if (quality) {
-    options.quality = quality
-  }
-  options.resource_type = "auto"
-  // console.log("OPTIONS", options)
-  return await cloudinary.uploader.upload(file.tempFilePath, options)
-}
+exports.uploadImageToCloudinary = async (file, folder) => {
+  const options = {
+    folder,
+    resource_type: "auto", // Automatically detect resource type
+    transformation: [
+      { width: 1200, crop: "scale" }, // Hardcoded width
+      { quality: 75 } // Hardcoded quality
+    ]
+  };
+
+  // Upload the image
+  return await cloudinary.uploader.upload(file.tempFilePath, options);
+};
